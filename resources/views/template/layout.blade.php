a4f1ee3496d7181da7a33a5badb435eb63356224
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-06-13 22:47:43 -0500

    Quitando public de la ruta de css y js ya que solo aplica cuando se ejecuta el proyecto desde localhost/public
    Con esto ya podemos hacer uso de los scripts y estilos

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -9,9 +9,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
     <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <script>
+        console.log('Esto es una prueba');
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
         ]) !!};
@@ -22,7 +23,7 @@
 <div class="app">
     @yield('content')
 </div>
-<script src="{{ asset('public/js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 
 @yield('script')
 </body>
