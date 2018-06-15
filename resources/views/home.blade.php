0fab5fec638392e905a84fb3e566a97ecf886c9e
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-06-14 20:34:29 -0500

    Agregando departamento
    Agregando el bonton para departamento, mostrar la tabla con los departamentos, creando los metodos apara poder almacenar y mostrar los departamentos.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,10 +42,28 @@
                     <div class="column text-center">
                         <h3>Departamentos</h3>
                     </div>
+                    <div>
+                        <a class="button is-success" @click="openModal('departure', 'create')">Agregar</a>
+                    </div>
                 </div>
                 <div class="columns">
                     <div class="column">
-                        Tabla de  departamentos
+                        Número de departamentos @{{departures.length}}
+                        <div v-if="!departures.length">
+                            no hay departamentos @{{departures.length}}
+                        </div>
+                        <table v-else class="table">
+                            <thead>
+                                <th>#</th>
+                                <th>Titulo</th>
+                            </thead>
+                            <tbody>
+                                <tr v-for="departure in departures">
+                                    <td>@{{ departure.id }}</td>
+                                    <td>@{{ departure.title }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -78,14 +96,146 @@
             <div class="column">Cargo 0</div>
         </div>
     </div>
+    <!-- Importando el modal departamento -->
+    @include('complementos.modal_departure')
 @endsection
 @section('script')
 <script>
     let elemento = new Vue({
         el: '.app',
+        mounted: function () {
+            // Las funciones que usemos dentro de mounted 
+            // se ejecutaran al inicio de la aplicación
+            this.allQuery();
+        },
         data: {
-            menu: 0
-        }
+            error: 'Probando que este tomando los datos',
+            menu: 0,
+            modalGeneral: 0,
+            titleModal: '',
+            messageModal: '',
+            modalDeparture: 0,
+            titleDeparture: '',
+            errorTitleDeparture: 0,
+            departures:[]
+        },
+        watch: {
+            // watch simplemente es decirle al sistema que este atento
+            // al cambio a una variable y haga algo cuando eso pasa
+            modalGeneral: function (value) {
+                if(!value) this.allQuery();
+            }
+        },
+        methods: {
+            closeModal() {
+                this.modalGeneral = 0;
+                this.titleModal = '';
+                this.messageModal = '';
+            },
+            createDeparture() {                
+                if (this.titleDeparture == '') {
+                    this.errorTitleDeparture = 1;
+                    return;
+                }
+                // Asociar this a la variable me, para evitar referenciar
+                // al objeto de axios.
+                let me = this;
+                
+                axios.post('{{route('departurecreate')}}', {
+                    'title': this.titleDeparture
+                })
+                .then(function (response) {
+                    me.titleDeparture = '';
+                    me.errorTitleDeparture = 0;
+                    me.modalDeparture = 0;
+                    me.closeModal();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            openModal(type, action, data = []) {
+                switch (type) {
+                    case "departure":
+                        {
+                            switch (action) {
+                                case 'create':
+                                    {
+                                        this.modalGeneral = 1;
+                                        this.titleModal = 'Creación de departamento';
+                                        this.messageModal = 'Ingrese el título del departamento';
+                                        this.modalDeparture = 1;
+                                        this.titleDeparture = '';
+                                        this.errorTitleDeparture = 0;
+                                        break;
+                                    }
+                                case 'update':
+                                    {
+                                        break;
+                                    }
+                                case 'delete':
+                                    {
+                                        break;
+                                    }
+
+                            }
+                            break;
+                        }
+                    case "position":
+                        {
+                            switch (action) {
+                                case 'create':
+                                    {
+
+                                        break;
+                                    }
+                                case 'update':
+                                    {
+                                        break;
+                                    }
+                                case 'delete':
+                                    {
+                                        break;
+                                    }
+
+                            }
+                            break;
+                        }
+                    case "employee":
+                        {
+                            switch (action) {
+                                case 'create':
+                                    {
+
+                                        break;
+                                    }
+                                case 'update':
+                                    {
+                                        break;
+                                    }
+                                case 'delete':
+                                    {
+                                        break;
+                                    }
+
+                            }
+                            break;
+                        }
+                }
+            },
+            allQuery() {
+                let me = this;
+                axios.get('{{ route('allQuery') }}')
+                    .then(function (response) {
+                        console.log(response);
+                        let answer = response.data;
+                        me.departures = answer.departures;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+            }
+        },
     })
 </script>
 @endsection
